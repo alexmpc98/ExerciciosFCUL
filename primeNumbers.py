fd36bbb3b69fa05ac7d5e0951a5ff0cfd947f846
alexmpc98 <48052322+alexmpc98@users.noreply.github.com>
2020-11-19 14:21:08 +0000

    Remoção de virgulas
    + Removação de virgulas

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,23 +1,23 @@
 # Numeros primos
 
-num = input();
-primeCounter = 0;
+num = input()
+primeCounter = 0
 
 while(int(num) != 0):
-    auxNum = int(num);
-    prime = True;
+    auxNum = int(num)
+    prime = True
     if(int(num) == 1):
-        prime = False;
+        prime = False
     while(auxNum > 1):
-        auxAux = auxNum - 1;
+        auxAux = auxNum - 1
         if(int(num) % auxAux == 0 and auxAux > 1):
-            prime = False;
-        auxNum = auxNum - 1;
+            prime = False
+        auxNum = auxNum - 1
     if(prime == True):
-        primeCounter = primeCounter + 1;
-        print(num + " é primo");
-        num = input();
+        primeCounter = primeCounter + 1
+        print(num + " é primo")
+        num = input()
     else:
-        print(num + " não é primo");
-        num = 0;
-        print("Introduziu " + str(primeCounter) + " primos");
+        print(num + " não é primo")
+        num = 0
+        print("Introduziu " + str(primeCounter) + " primos")
