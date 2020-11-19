13af35163bd235492e06a711bf5e54663926d043
alexmpc98 <48052322+alexmpc98@users.noreply.github.com>
2020-11-19 19:55:32 +0000

    Construção de ciclo
    + Construção do ciclo principal

--- shockCarts.py
+++ shockCarts.py
@@ -18,3 +18,25 @@ while(int(speedReduction) <= 0):
     print("Speed Reduction must be greater than 0")
     speedReduction = input()
 
+
+
+while(int(initialVelocity) >= 0):
+    while(int(shockCarInitialPosition) < int(dim)):
+        print(int(shockCarInitialPosition))
+        shockCarInitialPosition = int(shockCarInitialPosition) + int(initialVelocity)
+    shockCarInitialPosition = int(dim)
+    initialVelocity = int(initialVelocity) - int(speedReduction)
+    while(int(shockCarInitialPosition) > int(dim)*-1):
+        print(int(shockCarInitialPosition))
+        shockCarInitialPosition = int(shockCarInitialPosition) - int(initialVelocity)
+    shockCarInitialPosition = int(dim)*-1
+    initialVelocity = int(initialVelocity) - int(speedReduction)
+    print("Here -------")
+    print(int(initialVelocity))
+    print("-------------")
+
+
+
+
+
+
