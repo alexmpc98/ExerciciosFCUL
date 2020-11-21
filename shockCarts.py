4e3fdac68d22974b57df9dfcbbb9aabe47f656d8
Alex <alexandremp98@hotmail.com>
2020-11-21 16:20:56 +0000

    Alterações finais - PT5
    + Alterações finais:
     + Documento com o enunciado dos exercicios;
     + Simplificação do shockCarts.py (finalizado)

--- shockCarts.py
+++ shockCarts.py
@@ -1,38 +1,23 @@
-dim = input()
-while(int(dim) <= 0):
-    print("Value must be greater than 0")
-    dim = input()
+dim=int(input())
+x=int(input())
+velocidade=int(input())
+loss=int(input())
+speed=velocidade
 
-shockCarInitialPosition = input()
-while(int(shockCarInitialPosition) <= int(dim)*-1 or int(shockCarInitialPosition) >= int(dim)):
-    print("Position should be greater or smaller than dimension")
-    shockCarInitialPosition = input()
-
-initialVelocity = input()
-while(int(initialVelocity) <= 0):
-    print("Initial velocity must be greater than 0")
-    initialVelocity = input()
-
-speedReduction = input()
-while(int(speedReduction) <= 0):
-    print("Speed Reduction must be greater than 0")
-    speedReduction = input()
-
-
-
-while(int(initialVelocity) > 0):
-    while(int(shockCarInitialPosition) < int(dim) and int(initialVelocity) > 0):
-        print(int(shockCarInitialPosition))
-        shockCarInitialPosition = int(shockCarInitialPosition) + int(initialVelocity)
-    if(int(shockCarInitialPosition) >= int(dim)):
-        shockCarInitialPosition = int(dim)
-        initialVelocity = int(initialVelocity) - int(speedReduction)
-    while(int(shockCarInitialPosition) > int(dim)*-1 and int(initialVelocity) > 0):
-        print(int(shockCarInitialPosition))
-        shockCarInitialPosition = int(shockCarInitialPosition) - int(initialVelocity)
-    if(int(shockCarInitialPosition) <= int(dim)*-1):
-        shockCarInitialPosition = int(dim)*-1
-        initialVelocity = int(initialVelocity) - int(speedReduction)
-print(int(dim))
+while(velocidade>0):
+    print(x)
+    x=x+speed
+    if(x>=dim):
+        x=dim
+        speed=speed-loss
+        velocidade=speed
+        speed=-speed
+    elif(x<=-dim):
+        x=-dim
+        speed=speed+loss
+        velocidade=-speed
+        speed=-speed
+if(velocidade<=0) :
+    print(x)
     
     
