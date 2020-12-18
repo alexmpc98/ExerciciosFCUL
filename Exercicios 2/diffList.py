5388ef5292fafbfa67c1cffacd5a5b9da9c2c4db
Alex <alexandremp98@hotmail.com>
2020-12-18 14:39:19 +0000

    Separação por pastas + exercicios 2
    + DiffList

---
+++ Exercicios 2/diffList.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 16 20:11:31 2020
+
+@author: Alexandre
+"""
+
+def diff_list(lista1,lista2):
+    listaRetorno = []
+    if(len(lista1) == 0):
+        return listaRetorno
+    if(len(lista2) == 0 and len(lista1) > 0):
+        listaRetorno = lista1
+        return listaRetorno   
+    for a in lista1:
+        if a not in lista2:
+            listaRetorno.append(a)
+    return listaRetorno
+   
+
+
+lista1 = [9,7,5,3,7,1]
+lista2 = [1,11,77]
+print(diff_list(lista1,lista2))
+
+
